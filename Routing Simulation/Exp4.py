08742f9cd539a8a884a91f15c1cd72fd55602318
Eghonghonaye <eghonghoneigbe@gmail.com>
2018-10-18 21:47:22 +0200

    distnace dependence added

--- Routing Simulation/Exp4.py
+++ Routing Simulation/Exp4.py
@@ -1,21 +1,48 @@
-import os
+import os, random
 import networkx as nx
 import matplotlib.pyplot as plt
-import numpy
+import matplotlib.animation as animation
+import numpy, math
+#import seaborn as sns
 
 
 numberOfNodes = int(raw_input("How many nodes are in the graph?"))
 probability = float(raw_input("what is the probability of edge existence?"))
 
 g = nx.erdos_renyi_graph(numberOfNodes,probability)
-AdjacencyMatrix = nx.adjacency_matrix(g)
-AdjacencyMatrix = nx.to_numpy_matrix(g)
-AdjacencyMatrix = AdjacencyMatrix.tolist()
-#print(AdjacencyMatrix)
+pos = nx.spring_layout(g) #define graph layput so node positions stay the same from plot to plot
 
-#print(AdjacencyMatrix[1])
 
-  
+def eucledianDistanceBetweenNodes():
+	#this function takes the graph as input and finds eucledian distance between each nodes with a link
+	#this is used later when we delete links between nodes that are too far apart
+	#this adds a distance dependence to the random graph geenrated
+	lengths={}
+	for edge in g.edges():
+		startnode=edge[0] 
+		endnode=edge[1]
+		lengths[edge]=round(math.sqrt(((pos[endnode][1]-pos[startnode][1])**2)+((pos[endnode][0]-pos[startnode][0])**2)),2)
+	plt.bar(range(len(lengths.keys())), list(lengths.values()), color='g')
+	plt.show()
+	return lengths
+
+
+def editGraphDistance():
+	#this function deletes edges that are longer than 1/2 the maximum edge length that was randomly generated
+	lengths = eucledianDistanceBetweenNodes()
+	maximumDistance = max(list(lengths.values()))
+	#print (lengths)
+	for i,j in lengths.items():
+		if j > (maximumDistance/2):
+			#print(i,":",j)
+			g.remove_edge(i[0],i[1])
+			
+	#randomly assigning edge weights
+	for edge in g.edges():
+		startnode = edge[0]
+		endnode = edge[1]
+		g[startnode][endnode]['weight']=random.randint(1,11)
+
 
 def shortestPath(AdjacencyMatrix,sourceNode):
 	i = 0 
@@ -61,10 +88,6 @@ def shortestPath(AdjacencyMatrix,sourceNode):
 	return visitedList
 	
 	
-#initialise elements
-typeFlag, source, destination, sequence, metric, intermediary,data = 1,1,1,1,1,1,1
-packet = [typeFlag, source, destination, sequence, metric, intermediary,data]
-
 class adHocNode(object):
 	def __init__(self,MACAddress,rDestination =[],rSequence=[],rMetric=[],rNextHop=[]):
 		self.MACAddress = MACAddress
@@ -98,10 +121,6 @@ class adHocNode(object):
 			print("Update Packet")
 			#updateTable(Packet)
 		
-
-
-
- 
     
 def main():
 	
@@ -130,33 +149,74 @@ def main():
 	destination = int(raw_input("What is the destination?"))
 	
 	
+	#nx.draw(g,pos, node_color='green', edge_color='grey', with_labels = True, alpha = 0.5) #draw the network
+	
 	path.append(source)
 	while ((nodeList[source].rNextHop[destination])!=(nodeList[source].rNextHop[source])):
 		nextHop = nodeList[source].rNextHop[destination]
 		path.append(nextHop)
-		#print(nextHop)
 		nodeList[source].sendPacket(nodeList[source].createPacket(1,source,destination,[],[],nextHop,"yes") )
 		source = nextHop
+	print (path)
 	
+	path1=[]
 	
-	for node in g:
-		if node in path:
-			color_map.append('blue')
-		else: color_map.append('green') 
-	
-	
-	print (path)	
-	
-	pos = nx.spring_layout(g)
-	path_edges = zip(path,path[1:])
-	
-	nx.draw(g,pos, node_color='green', with_labels = True)
-	#nx.draw(g, node_color = color_map, with_labels=True)
-	nx.draw_networkx_nodes(g,pos,nodelist=path,node_color='blue')
-	nx.draw_networkx_edges(g,pos,edgelist=path_edges,edge_color='purple',width=5)
-	plt.draw()
-	plt.show()
+	for i in range(len(path)):
+		plt.gcf().clear()
+		path1.append(path[i])
+		#print("inloop")
+		nx.draw(g,pos, node_color='green', edge_color='grey', with_labels = True, alpha = 0.5)
+		path_edges = zip(path,path[1:])
+		nx.draw_networkx_nodes(g,pos,nodelist=path1,node_color='blue')
+		nx.draw_networkx_edges(g,pos,edgelist=path_edges,edge_color='purple',width=1)
+			
+		plt.draw()
+		plt.pause(1)
 		
+	plt.show()
+			
+	#return path	
+	
+
+def plotPath(i):
+	
+	path_edges = zip(path,path[1:])
+	nx.draw_networkx_nodes(g,pos,nodelist=path[i],node_color='blue')
+	#nx.draw_networkx_edges(g,pos,edgelist=path_edges[i],edge_color='purple',width=5)
+		
+
+
+
+
+
+editGraphDistance()
+
+#this series of actions provides the adjacency matrix as a list of lists
+#so we can iterated through them for paths and rputing later
+AdjacencyMatrix = nx.adjacency_matrix(g)
+AdjacencyMatrix = nx.to_numpy_matrix(g)
+AdjacencyMatrix = AdjacencyMatrix.tolist()
+
+path = [] #path that packet takes. saved as a list for the plot
+
+# Build plot
+#fig, ax = plt.subplots(figsize=(6,4))
+
+labels = nx.get_edge_attributes(g,'weight')
+nx.draw(g,pos, node_color='green', edge_color='grey', edge_labels=labels, with_labels = True, alpha = 0.5)
+plt.show()  
+
+
+#initialise elements
+typeFlag, source, destination, sequence, metric, intermediary,data = 1,1,1,1,1,1,1
+packet = [typeFlag, source, destination, sequence, metric, intermediary,data]
 
 main()
 
+
+
+
+#ani = animation.FuncAnimation(fig, plotPath, frames = range(len(path)),repeat=True)
+#plt.show()
+#plt.draw()
+#plt.show()
